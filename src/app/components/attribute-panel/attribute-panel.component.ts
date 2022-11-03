83c3707a6587e09b34585f3132184396f1e0ce54
Chris Peach <cpeach@mitek.co.uk>
2022-11-03 12:39:27 +0000

    added stuff to info panel

---
+++ src/app/components/attribute-panel/attribute-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-attribute-panel',
+  templateUrl: './attribute-panel.component.html',
+  styleUrls: ['./attribute-panel.component.scss']
+})
+export class AttributePanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
